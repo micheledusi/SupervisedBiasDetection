fecf2b35bfd965f2b4c1ce5716700b238d33b52e
MicheleDusi <micheledusi.618@gmail.com>
2023-01-19 18:16:39 +0100

    Implement first example with real embeddings

--- src/model/regression/linear_regressor.py
+++ src/model/regression/linear_regressor.py
@@ -21,6 +21,7 @@ from pathlib import Path
 directory = Path(__file__)
 sys.path.append(str(directory.parent.parent.parent))
 from model.regression.abstract_regressor import AbstractRegressor
+from utility.cache import CacheManager
 
 
 class TorchLinearRegression(torch.nn.Module):
@@ -55,7 +56,7 @@ class LinearRegressor(AbstractRegressor):
     input_size: int = 768
     output_size: int = 1
     learning_rate: float = 0.005
-    epochs: int = 100
+    epochs: int = 1000
     
     def __init__(self) -> None:
         super().__init__()
@@ -74,13 +75,13 @@ class LinearRegressor(AbstractRegressor):
         if torch.cuda.is_available():
             inputs.to("cuda")
 
-        values_set: set[str] = set(dataset['value'])
-        features = Features({'label': ClassLabel(num_classes=len(values_set), names=list(values_set))})
+        values_set: list[str] = sorted(set(dataset['value']))   # In this way, the order of the values is computed according to the alphabetical order
+        features = Features({'label': ClassLabel(num_classes=len(values_set), names=values_set)})
         labels_ds = Dataset.from_dict({"label": dataset['value']}, features=features) 
         labels_ds = labels_ds.with_format("torch", device=torch.device)
         labels = Variable(labels_ds['label'].unsqueeze(1).float())
+        # TODO: Convert labels to -1 and +1
 
-        print(labels.data)
         # Note: we add the unsqueeze(1) to convert the tensor from shape (batch_size,) to (batch_size, 1)
 
         for epoch in range(self.epochs):
@@ -94,16 +95,15 @@ class LinearRegressor(AbstractRegressor):
             loss.backward()
             # Updating the parameters
             optimizer.step()
-
-            print('Epoch {} => loss = {}'.format(epoch, loss.item()))
+            # print('Epoch {} => loss = {}'.format(epoch, loss.item()))
     
 
     def predict(self, dataset: Dataset) -> Dataset:
-        def map_fn(example):
-            inputs = Variable(example['embedding'])
-            example['prediction'] = self.model(inputs)
-            return example
-        return dataset.map(map_fn, batched=True)
+        def predict_fn(sample):
+            inputs = Variable(sample['embedding'])
+            sample['prediction'] = self.model(inputs)
+            return sample
+        return dataset.map(predict_fn, batched=True)
 
 
     @property
@@ -113,62 +113,64 @@ class LinearRegressor(AbstractRegressor):
 
 
 if __name__ == '__main__':
-    import numpy as np
-
     torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Testing the linear regression model
-
-    # Prepare the training dataset
-    ds: Dataset = Dataset.from_dict({'word': [], 'embedding': [], 'value': []})
-    for i in range(500):
-        # (1) sample
-        fake_embedding = np.random.rand(LinearRegressor.input_size)
-        fake_embedding[0] = 1.0
-        fake_embedding[1] = 1.0
-        fake_embedding[2] = 1.0
-        fake_value = 'a'
-        ds = ds.add_item({'word': "AAA", 'embedding': fake_embedding, 'value': fake_value})
-        # (0) sample
-        fake_embedding = np.random.rand(LinearRegressor.input_size)
-        fake_embedding[0] = 0.0
-        fake_embedding[1] = 0.0
-        fake_embedding[2] = 0.0
-        fake_value = 'b'
-        ds = ds.add_item({'word': "BBB", 'embedding': fake_embedding, 'value': fake_value})
-    ds = ds.with_format("torch")
-    print("Training dataset: ", ds)
-    print("First element: ", ds[0])
-    print("First element embedding size: ", ds[0]['embedding'].size())
-
-    print("Embedding column: ", ds['embedding'])
-    print("It's a tensor? >> ", isinstance(ds['embedding'], torch.Tensor))
+    # Parameters
+    tested_property = 'quality'
+    input_words_file = 'data/stereotyped-p/quality/words-01.csv'
+    input_templates_file = 'data/stereotyped-p/quality/templates-01.csv'
+    param_select_templates = 'all'
+    param_average_templates = True
+    param_average_tokens = True
     
-    # Train the model
-    reg_model = LinearRegressor()
-    reg_model.train(ds)
+    # Disk management for embedding datasets
+    cacher = CacheManager()
+    name = 'quality_words'
+    group = 'embedding'
+    metadata = {
+        'stereotyped_property': tested_property,
+        'input_words': input_words_file,
+        'input_templates': input_templates_file,
+        'select_templates': param_select_templates,
+        'average_templates': param_average_templates,
+        'average_tokens': param_average_tokens,
+    }
 
-    # Prepare the test dataset
-    ds: Dataset = Dataset.from_dict({'word': [], 'embedding': []})
-    for i in range(2):
-        # (1) sample
-        fake_embedding = np.random.rand(LinearRegressor.input_size)
-        fake_embedding[0] = 2.0
-        fake_embedding[1] = 2.0
-        fake_embedding[2] = 2.0
-        ds = ds.add_item({'word': "AAA", 'embedding': fake_embedding})
-        # (0) sample
-        fake_embedding = np.random.rand(LinearRegressor.input_size)
-        fake_embedding[0] = 0.0
-        fake_embedding[1] = 0.0
-        fake_embedding[2] = 0.0
-        ds = ds.add_item({'word': "BBB", 'embedding': fake_embedding})
-    ds = ds.with_format("torch")
-    print("Test dataset: ", ds)
+    if cacher.exists(name, group, metadata):
+        print("Loading the embedding dataset from the cache...")
+        embedding_dataset = cacher.load(name, group, metadata)
+    else:
+        print("Embedding the words...")
+        from model.embedding.word_embedder import WordEmbedder
+        # Loading the datasets
+        templates: Dataset = Dataset.from_csv('data/stereotyped-p/quality/templates-01.csv')
+        words: Dataset = Dataset.from_csv('data/stereotyped-p/quality/words-01.csv').shuffle(seed=42)
+        # Creating the word embedder
+        word_embedder = WordEmbedder(select_templates='all', average_templates=True, average_tokens=True)
+        # Embedding a word
+        embedding_dataset = word_embedder.embed(words, templates)
+        # Caching the embedding dataset
+        cacher.save(embedding_dataset, name, group, metadata)
+
+    # Squeezing the embeddings to remove the template dimension and the token dimension
+    def squeeze_fn(sample):
+        sample['embedding'] = sample['embedding'].squeeze()
+        return sample
+    embedding_dataset = embedding_dataset.map(squeeze_fn, batched=True)
+
+    # Splitting the dataset into train and test
+    embedding_dataset = embedding_dataset.train_test_split(test_size=0.2, shuffle=True, seed=42)
+
+    # Using the embeddings to train the model
+    reg_model = LinearRegressor()
+    reg_model.train(embedding_dataset['train'])
 
     # Predict the values
-    ds = reg_model.predict(ds)
-    print("Predictions: ", ds['prediction'])
-
-    # Print the weights
-    print("What are the weights? ", reg_model.features_relevance)
+    results = reg_model.predict(embedding_dataset['test'])
+    score: int = 0
+    for result in results:
+        predicted_value = 'negative' if result['prediction'] < 0.5 else 'positive'
+        guessed = predicted_value == result['value']
+        print(f"{result['word']:20s} => ", guessed, f"({predicted_value} vs {result['value']})")
+        score += 1 if guessed else 0
+    print(f"Score: {score}/{len(results)} ({score/len(results)*100:.2f}%)")
