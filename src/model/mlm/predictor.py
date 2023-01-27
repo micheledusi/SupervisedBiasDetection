d250be0a5336624200c501cb4a7a85608bc98929
MicheleDusi <micheledusi.618@gmail.com>
2023-01-27 10:02:25 +0100

    Implement filtering and fix bugs in MLM prediction

--- src/model/mlm/predictor.py
+++ src/model/mlm/predictor.py
@@ -15,9 +15,10 @@ from pathlib import Path
 directory = Path(__file__)
 sys.path.append(str(directory.parent.parent))
 
-from utility.const import DEFAULT_BERT_MODEL_NAME, TOKEN_MASK
+from utility import const
 from data_processing.sentence_maker import replace_stereotyped_word, mask_protected_word
 from data_processing.sentence_maker import get_generation_datasets
+from model.embedding.word_embedder import WordEmbedder
 
 
 class MLMPredictor:
@@ -28,9 +29,10 @@ class MLMPredictor:
 	__softmax = torch.nn.Softmax(dim=-1)
 
 	def __init__(self) -> None:
-		self.tokenizer = AutoTokenizer.from_pretrained(DEFAULT_BERT_MODEL_NAME)
-		self.model = AutoModelForMaskedLM.from_pretrained(DEFAULT_BERT_MODEL_NAME)
-
+		self.tokenizer = AutoTokenizer.from_pretrained(const.DEFAULT_BERT_MODEL_NAME)
+		self.model = AutoModelForMaskedLM.from_pretrained(const.DEFAULT_BERT_MODEL_NAME)
+		if torch.cuda.is_available():
+			self.model.cuda()
 
 	def predict(self, sentences: str | list[str], target: str) -> float:
 		"""
@@ -47,8 +49,8 @@ class MLMPredictor:
 
 		# Asserting that each sentence contains the MASK token
 		for sentence in sentences:
-			if TOKEN_MASK not in sentence:
-				raise Exception(f"The sentence '{sentence}' does not contain the mask token ({TOKEN_MASK}).")
+			if const.TOKEN_MASK not in sentence:
+				raise Exception(f"The sentence '{sentence}' does not contain the mask token ({const.TOKEN_MASK}).")
 			
 		inputs = self.tokenizer(sentences, padding=True, truncation=False, return_tensors="pt")
 		mask_token_index = torch.where(inputs["input_ids"] == self.tokenizer.mask_token_id)[1]	# Getting the index of the mask token for each sentence (=batch)
@@ -77,47 +79,58 @@ class MLMPredictor:
 		target_index = self.tokenizer.convert_tokens_to_ids(target)
 		return vocabulary_scores[target_index].item()
 
+	def compute_scores(self, protected_property: str, stereotyped_property: str, generation_file_id: int = 1) -> Dataset:
+		# Retrieve the datasets for MLM
+		pp_words, sp_words, templates = get_generation_datasets(protected_property, stereotyped_property, generation_file_id)
 
-PROTECTED_PROPERTY: str = "religion"
-STEREOTYPED_PROPERTY: str = "quality"
+		# Prepare the dataset for storing the scores
+		resulting_scores: Dataset = Dataset.from_dict({})
+
+		# Preparing an auxiliary embedder that can be used to discard long words (i.e. words that are tokenized in more than X tokens)
+		embedder = WordEmbedder()
+		if const.DEFAULT_DISCARD_LONGER_WORDS:
+			print("Filtering the stereotyped words...")
+			sp_words = sp_words.filter(lambda x: embedder.get_tokens_number(x['word']) <= const.DEFAULT_MAX_TOKENS_NUMBER)
+		
+		# For each stereotyped word
+		print("Computing the scores...")
+		for sw in tqdm(sp_words):
+
+			# For each template, we insert the stereotyped word in the slot
+			sentences_pairs = [replace_stereotyped_word(sent, sw) for sent in templates['template']]
+			sentences = [sent_pair[0] for sent_pair in sentences_pairs if sent_pair[1] is True]
+
+			# Prepare the dictionary for storing the scores for each protected value
+			protected_scores: dict[str, float] = {"stereotyped_word": sw['word'], "stereotype_value": sw['value']}
+
+			for pw in pp_words:
+				# For each protected word, we mask it in the sentences
+				masked_sentences = [mask_protected_word(sent) for sent in sentences]
+				# Then, we predict the probability that the protected word is inserted in the MASK slot
+				protected_scores[pw['value']] = self.predict(masked_sentences, pw['word'])
+
+			resulting_scores = resulting_scores.add_item(protected_scores)
+		# Formatting with PyTorch
+		resulting_scores = resulting_scores.with_format("torch", device=const.DEVICE)
+
+		# Pairing the scores relative to two protected values, and computing the polarization
+		values_columns = resulting_scores.column_names[2:]
+		for i, column1 in enumerate(values_columns):
+			for column2 in values_columns[i+1:]:
+				if column1 != column2:
+					values: torch.Tensor = resulting_scores[column1].sub(resulting_scores[column2])
+					resulting_scores = resulting_scores.add_column(f"polarization_{column1}_{column2}", values.tolist())
+		
+		return resulting_scores
+
 
 if __name__ == "__main__":
-	# Retrieve the datasets
-	pp_words, sp_words, templates = get_generation_datasets(PROTECTED_PROPERTY, STEREOTYPED_PROPERTY, 1)
+	# Constants
+	PROTECTED_PROPERTY: str = "religion"
+	STEREOTYPED_PROPERTY: str = "quality"
 
-	print("Number of protected words:", len(pp_words))
-	print("Number of stereotyped words:", len(sp_words))
-	print("Number of templates:", len(templates))
-
-	model = MLMPredictor()
-	resulting_scores: Dataset = Dataset.from_dict({})
-
-	# For each stereotyped word
-	for sw in tqdm(sp_words):
-
-		# For each template, we insert the stereotyped word in the slot
-		sentences_pairs = [replace_stereotyped_word(sent, sw) for sent in templates['template']]
-		sentences = [sent_pair[0] for sent_pair in sentences_pairs if sent_pair[1] is True]
-
-		# Prepare the dictionary for storing the scores for each protected value
-		protected_scores: dict[str, float] = {"stereotyped_word": sw['word'], "stereotype_value": sw['value']}
-
-		for pw in pp_words:
-			masked_sentences = [mask_protected_word(sent) for sent in sentences]
-			score: float = model.predict(masked_sentences, pw['word'])
-
-			# We assume that a protected value is represented by only one protected word
-			protected_scores[pw['value']] = score
-
-		resulting_scores = resulting_scores.add_item(protected_scores)
-
-	print("Results: ", resulting_scores)
-
-	# Pairing the scores with the protected values
-	for column1 in resulting_scores.column_names[2:]:
-		for column2 in resulting_scores.column_names[2:]:
-			if column1 != column2:
-				resulting_scores[f"polarization_{column1}_{column2}"] = resulting_scores[column1] - resulting_scores[column2]
+	# Computing the scores
+	resulting_scores = MLMPredictor().compute_scores(PROTECTED_PROPERTY, STEREOTYPED_PROPERTY)
 
 	# Saving the resulting scores
 	output_file = 'mlm_scores.csv'
