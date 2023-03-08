60e9281d20a93a6a6d5dc2bd7f9a433b29346f6e
MicheleDusi <micheledusi.618@gmail.com>
2023-03-08 01:09:10 +0100

    Update "dimensionality reduction" experiment
    - Use new implementation
    - Use new plotting
    - Compare protected and stereotyped properties
    - Predict values

--- src/experiments/dim_reduction.py
+++ src/experiments/dim_reduction.py
@@ -10,25 +10,25 @@
 
 import os
 from datasets import Dataset
+import torch
 
 from data_processing.data_reference import PropertyDataReference
 from experiments.base import Experiment
 from model.classification.base import AbstractClassifier
+from model.classification.factory import ClassifierFactory
 from model.reduction.weights import WeightsSelectorReducer
-from model.classification.linear import LinearClassifier
 from model.reduction.composite import CompositeReducer
 from model.reduction.pca import TrainedPCAReducer
 from utils.config import Configurations, Parameter
-from view.plotter.scatter import ScatterPlotter, emb2plot
-from utils.const import DEFAULT_TEMPLATES_SELECTED_NUMBER, DEFAULT_MAX_TOKENS_NUMBER
+from view.plotter.scatter import ScatterPlotter
 
-PROTECTED_PROPERTY: PropertyDataReference = ("religion", "protected", 3, 0)
-STEREOTYPED_PROPERTY: PropertyDataReference = ("verb", "stereotyped", 1, 1)
+PROTECTED_PROPERTY = PropertyDataReference("religion", "protected", 1, 0)
+STEREOTYPED_PROPERTY = PropertyDataReference("verb", "stereotyped", 1, 1)
 
 configs = Configurations({
 	Parameter.MAX_TOKENS_NUMBER: 'all',
 	Parameter.TEMPLATES_SELECTED_NUMBER: 3,
-	Parameter.CLASSIFIER_TYPE: 'linear',
+	Parameter.CLASSIFIER_TYPE: 'svm',
 	Parameter.CROSSING_STRATEGY: 'pppl',
 	Parameter.POLARIZATION_STRATEGY: 'difference',
 })
@@ -56,35 +56,62 @@ class DimensionalityReductionExperiment(Experiment):
 		
 		# Getting embeddings
 		protected_embedding_dataset, stereotyped_embedding_dataset = Experiment._get_embeddings(PROTECTED_PROPERTY, STEREOTYPED_PROPERTY, configs)
+		prot_embs = protected_embedding_dataset['embedding']
+		ster_embs = stereotyped_embedding_dataset['embedding']
+		embs = torch.cat((prot_embs, ster_embs), dim=0)
+		print("Embeddings obtained")
+		print("Total number of embeddings:", len(embs))
+		num_prot = len(prot_embs)
+		num_ster = len(ster_embs)
+		print(f"({num_prot} protected words + {num_ster} stereotyped words = {num_prot + num_ster} total words)")
 
 		# Reducing the dimensionality of the embeddings
 		midstep: int = 50
 
 		# 1. Reduction based on the weights of the classifier
 		# 2. Reduction based on PCA
-		classifier: AbstractClassifier = LinearClassifier()
+		classifier: AbstractClassifier = ClassifierFactory.create(configs)
 		classifier.train(protected_embedding_dataset)
 		reducer_1 = WeightsSelectorReducer.from_classifier(classifier, output_features=midstep)
-		reduced_protected_embeddings = reducer_1.reduce(protected_embedding_dataset['embedding'])
+		reduced_midstep_prot_embs = reducer_1.reduce(prot_embs)
 
-		reducer_2: TrainedPCAReducer = TrainedPCAReducer(reduced_protected_embeddings, output_features=2)
-
+		reducer_2: TrainedPCAReducer = TrainedPCAReducer(reduced_midstep_prot_embs, output_features=2)
 		reducer = CompositeReducer([
 			reducer_1,
 			reducer_2
 		])
 
 		# Reducing the embeddings
-		results = reducer.reduce(stereotyped_embedding_dataset['embedding'])
-		print("Results: ", results)
-		print("Results shape: ", results.shape)
+		reduced_prot_embs: torch.Tensor = reducer.reduce(prot_embs)
+		reduced_ster_embs: torch.Tensor = reducer.reduce(ster_embs)
+		reduced_embs: torch.Tensor = torch.cat((reduced_prot_embs, reduced_ster_embs), dim=0)
+		print("Reduction performed")
+		print("Reduced protected embeddings shape:", reduced_prot_embs.shape)
+		print("Reduced stereotyped embeddings shape:", reduced_ster_embs.shape)
+		print("Reduced total embeddings shape:", reduced_embs.shape)
+
+		# Predicting the values with the classifier
+		prot_prediction_dataset: Dataset = classifier.evaluate(protected_embedding_dataset)
+		ster_prediction_dataset: Dataset = classifier.evaluate(stereotyped_embedding_dataset)
+		predictions: torch.Tensor = torch.cat((prot_prediction_dataset['prediction'], ster_prediction_dataset['prediction']), dim=0)
+		predicted_values: list[str] = [classifier.classes[p] for p in predictions]
 		
-		# Showing the results
-		results_ds = Dataset.from_dict({'word': stereotyped_embedding_dataset['word'], 'embedding': results, 'value': stereotyped_embedding_dataset['value']})
-		plot_data: Dataset = emb2plot(results_ds)
+		# Aggregating the results to show them
+		results_ds = Dataset.from_dict({
+			'word': protected_embedding_dataset['word'] + stereotyped_embedding_dataset['word'],
+			'type': ['protected'] * num_prot + ['stereotyped'] * num_ster,
+			'value': protected_embedding_dataset['value'] + stereotyped_embedding_dataset['value'],
+			'x': reduced_embs[:, 0],
+			'y': reduced_embs[:, 1],
+			'prediction': predictions,
+			'predicted_protected_value': predicted_values,
+			})
+
 		# If the directory does not exist, it will be created
 		folder: str = f'results/{PROTECTED_PROPERTY.name}-{STEREOTYPED_PROPERTY.name}'
 		if not os.path.exists(folder):
 			os.makedirs(folder)
-		plot_data.to_csv(folder + f'/reduced_scatter_data_TM{DEFAULT_TEMPLATES_SELECTED_NUMBER}_TK{DEFAULT_MAX_TOKENS_NUMBER}_N{midstep}.csv', index=False)
-		# ScatterPlotter(plot_data).show()
+		configs_descriptor: str = configs.subget(Parameter.MAX_TOKENS_NUMBER, Parameter.TEMPLATES_SELECTED_NUMBER, Parameter.CLASSIFIER_TYPE).to_abbrstr()
+		results_ds.to_csv(folder + f'/reduced_data_{configs_descriptor}_N{midstep}.csv', index=False)
+		
+		ScatterPlotter(results_ds, color_col='predicted_protected_value').show()
--- src/view/plotter/scatter.py
+++ src/view/plotter/scatter.py
@@ -10,46 +10,20 @@
 from datasets import Dataset
 import matplotlib.pyplot as plt
 
-
-def emb2plot(embeddings: Dataset) -> Dataset:
-    """
-    Converts a dataset with reduced embeddings into a dataset with the right information to be plotted.
-    It requires:
-    - an 'embedding' column, to be split into 'x' and 'y'.
-    - a 'word' column, to be converted into 'label'
-    - a 'value' column, to be converted into 'color'
-
-    :param embeddings: The dataset with embeddings information to be plotted.
-    :return: A dataset that can be plotted by the ScatterPlotter class.
-    """
-    kvals: dict[str, int] = {}
-    def extract_info_fn(sample):
-        coords = sample["embedding"]
-        assert len(coords) == 2, "Cannot draw a bidimensional plot with embedding of size = {}".format(len(coords))
-        sample[ScatterPlotter.DEFAULT_COLUMN_X] = coords[0]
-        sample[ScatterPlotter.DEFAULT_COLUMN_Y] = coords[1]
-        # Label
-        if ScatterPlotter.DEFAULT_COLUMN_LABEL not in sample:
-            sample[ScatterPlotter.DEFAULT_COLUMN_LABEL] = sample['word']
-        # Color
-        if ScatterPlotter.DEFAULT_COLUMN_COLOR not in sample:
-            # Mapping each key to a unique integer
-            if sample['value'] not in kvals:
-                kvals[sample['value']] = len(kvals)
-            sample[ScatterPlotter.DEFAULT_COLUMN_COLOR] = kvals[sample['value']]
-        return sample
-
-    return embeddings.map(extract_info_fn, remove_columns=['embedding', 'word', 'value'], num_proc=1)
+DEFAULT_COLUMN_X = "x"
+DEFAULT_COLUMN_Y = "y"
+DEFAULT_COLUMN_LABEL = "word"
+DEFAULT_COLUMN_COLOR = "value"
 
 
 class ScatterPlotter:
-
-    DEFAULT_COLUMN_X = "x"
-    DEFAULT_COLUMN_Y = "y"
-    DEFAULT_COLUMN_LABEL = "label"
-    DEFAULT_COLUMN_COLOR = "color"
-
-    def __init__(self, data: Dataset, title: str = None) -> None:
+    """
+    This class is used to plot the embeddings.
+    The only requirement is that the dataset contains the columns "x" and "y".
+    """
+    def __init__(self, data: Dataset, title: str = None, 
+                 x_col: str = DEFAULT_COLUMN_X, y_col: str = DEFAULT_COLUMN_Y, 
+                 label_col: str = DEFAULT_COLUMN_LABEL, color_col: str = DEFAULT_COLUMN_COLOR) -> None:
         """
         The initializer for the ScatterPlotter class.
 
@@ -57,18 +31,48 @@ class ScatterPlotter:
         :param title: The title of the plot.
         """
         self._title = title
-        self._x = data[self.DEFAULT_COLUMN_X]
-        self._y = data[self.DEFAULT_COLUMN_Y]
-        self._labels = data[self.DEFAULT_COLUMN_LABEL]
-        self._colors = data[self.DEFAULT_COLUMN_COLOR]
+        self._x = data[x_col]
+        self._y = data[y_col]
+        self._labels = data[label_col] if label_col in data.column_names else []
+
+        # Check if the data type of the color column is int
+        colortype = 'int'
+        for i in range(len(data)):
+            if not isinstance(data[color_col][i], int):
+                colortype = 'symbolic'
+                break
+
+        if colortype == 'int':
+            self._colors = data[color_col]
+        else:
+            kvals: dict[str, int] = {}
+            self._colors = []
+            for color_sample in data[color_col]:
+                if color_sample not in kvals:
+                    kvals[color_sample] = len(kvals)
+                self._colors.append(kvals[color_sample])
 
     def show(self) -> None:
         """
         This method shows the plot.
         """
-        plt.scatter(self._x, self._y, c=self._colors)
-        for i, txt in enumerate(self._labels):
-            plt.annotate(txt, (self._x[i], self._y[i]))
+        # Plotting
+        if not self._colors:
+            print("No colors to show.")
+            plt.scatter(self._x, self._y)
+        else:
+            plt.scatter(self._x, self._y, c=self._colors)
+
+        # Adding labels
+        if not self._labels:
+            print("No labels to show.")
+        else:
+            for i, txt in enumerate(self._labels):
+                plt.annotate(txt, (self._x[i], self._y[i]))
+
+        # Adding title
         if self._title is not None:
             plt.title(self._title)
+
+        # Showing
         plt.show()
