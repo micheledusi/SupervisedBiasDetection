12f7a68a24872018054c4ace4f8de3054e412c46
MicheleDusi <micheledusi.618@gmail.com>
2023-07-18 11:36:24 +0200

    Update main to include experiments

--- src/main.py
+++ src/main.py
@@ -10,10 +10,12 @@
 import torch
 from datasets import disable_caching
 
+from experiments.classification import ClassificationExperiment
 from experiments.dim_reduction import DimensionalityReductionExperiment
+from experiments.dim_reduction_comparison import DimensionalityReductionsComparisonExperiment
 from experiments.midstep_analysis_2 import MidstepAnalysis2Experiment
 from experiments.midstep_chi_squared import MidstepAnalysisChiSquared
-from experiments.classification import ClassificationExperiment
+from experiments.separation import SeparationExperiment
 
 # Libraries setup
 torch.manual_seed(42)
@@ -21,10 +23,14 @@ torch.manual_seed(42)
 
 if __name__ == "__main__":
 	
-	# MidstepAnalysisChiSquared().run()
+	# ClassificationExperiment().run()
 	
+	DimensionalityReductionExperiment().run()
+
+	# DimensionalityReductionsComparisonExperiment().run()
+
 	# MidstepAnalysis2Experiment().run()
 
-	# DimensionalityReductionExperiment().run()
+	# MidstepAnalysisChiSquared().run()
 
-	ClassificationExperiment().run()
+	# SeparationExperiment().run()
