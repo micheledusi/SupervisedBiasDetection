58a44c9cfbc887b651dd8f9614e22ee06eb23154
MicheleDusi <micheledusi.618@gmail.com>
2023-10-11 14:04:29 +0200

    Change main script

--- src/main.py
+++ src/main.py
@@ -23,26 +23,27 @@ torch.manual_seed(42)
 disable_caching()
 logging.set_verbosity_error()
 
-PROTECTED_PROPERTY = PropertyDataReference("religion", 2, 1)
+PROTECTED_PROPERTY = PropertyDataReference("religion", 1, 1)
 STEREOTYPED_PROPERTY = PropertyDataReference("quality", 1, 1)
-MIDSTEP: int = 42
+MIDSTEP: int = 62
 
 if __name__ == "__main__":
 
 	# 
 	### Regarding one single property	
 	#
-	# ClassificationExperiment().run(prot_prop=PROTECTED_PROPERTY)
-	# SeparationExperiment().run(prot_prop=PROTECTED_PROPERTY, midstep=MIDSTEP)
+	ClassificationExperiment().run(prot_prop=PROTECTED_PROPERTY)
+	SeparationExperiment().run(prot_prop=PROTECTED_PROPERTY, midstep=MIDSTEP)
 	
 	#
 	### Regarding two properties and their relationship, with a specific midstep
 	#
 	DimensionalityReductionExperiment().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY, midstep=MIDSTEP)
-	# DimensionalityReductionsComparisonExperiment().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY, midstep=MIDSTEP)
+	DimensionalityReductionsComparisonExperiment().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY, midstep=MIDSTEP)
 
 	#
 	### Regarding two properties and their relationship, for all midsteps
 	#
 	MidstepAnalysisCorrelation().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY)
 	MidstepAnalysisChiSquared().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY)
+	
