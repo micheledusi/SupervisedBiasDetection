171241a6dec5fb286d90eb581534aa40c395a331
micheledusi <michele.dusi@unibs.it>
2024-01-25 15:57:35 +0000

    Comment unused experiments

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 #   Date:    2023               #
 # - - - - - - - - - - - - - - - #
 
-# This module contains the launcher of the project.
+# This module contains the launcher of the project
 
 import torch
 from datasets import disable_caching, logging
@@ -32,18 +32,18 @@ if __name__ == "__main__":
 	# 
 	### Regarding one single property	
 	#
-	ClassificationExperiment().run(prot_prop=PROTECTED_PROPERTY)
+	# ClassificationExperiment().run(prot_prop=PROTECTED_PROPERTY)
 	SeparationExperiment().run(prot_prop=PROTECTED_PROPERTY, midstep=MIDSTEP)
 	
 	#
 	### Regarding two properties and their relationship, with a specific midstep
 	#
 	DimensionalityReductionExperiment().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY, midstep=MIDSTEP)
-	DimensionalityReductionsComparisonExperiment().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY, midstep=MIDSTEP)
+	# DimensionalityReductionsComparisonExperiment().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY, midstep=MIDSTEP)
 
 	#
 	### Regarding two properties and their relationship, for all midsteps
 	#
-	MidstepAnalysisCorrelation().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY)
+	# MidstepAnalysisCorrelation().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY)
 	MidstepAnalysisChiSquared().run(prot_prop=PROTECTED_PROPERTY, ster_prop=STEREOTYPED_PROPERTY)
 	
