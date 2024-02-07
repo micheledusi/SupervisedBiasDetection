057149de39076179ab064120e8584c0e501a2abc
Michele Dusi <michele.dusi@unibs.it>
2024-02-07 16:52:35 +0100

    Comment deprecated patterns

--- src/data_processing/pattern.py
+++ src/data_processing/pattern.py
@@ -9,16 +9,17 @@
 
 import re
 
+# Deprecated patterns
+# PP_PATTERN = r'(\[PROT\-WORD(?:\:([A-Za-z\-]+))?\])'
+# PP_PATTERN: re.Pattern[str] = re.compile(PP_PATTERN)
 
-PP_PATTERN = r'(\[PROT\-WORD(?:\:([A-Za-z\-]+))?\])'
-PP_PATTERN: re.Pattern[str] = re.compile(PP_PATTERN)
-
-SP_PATTERN = r'(\[STER\-WORD(?:\:([A-Za-z\-]+))?\])'
-SP_PATTERN: re.Pattern[str] = re.compile(SP_PATTERN)
-# These pattern will extract two groups:
-#   - The first group will be the whole "protected word" mask, that is the part that's going to be replaced.
-#   - The second group will be the word descriptor, that is going to be used to select the word.
+# Deprecated patterns
+# SP_PATTERN = r'(\[STER\-WORD(?:\:([A-Za-z\-]+))?\])'
+# SP_PATTERN: re.Pattern[str] = re.compile(SP_PATTERN)
 
 PATTERN = r'(\[WORD(?:\:([A-Za-z\-]+))?\])'
 PATTERN: re.Pattern[str] = re.compile(PATTERN)
-# This pattern is the generalization of the previous two patterns.
+""" This pattern will extract two groups:
+   - The first group will be the whole "protected word" mask, that is the part that's going to be replaced.
+   - The second group will be the word descriptor, that is going to be used to select the word.
+"""
